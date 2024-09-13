805adeb61f8ae1f402a0cf91e8e9e0f124bd6c80
Félix Piédallu <felix.piedallu@semtech.com>
2024-09-13 16:59:03 +0200

    Checkout apps repo for make_readme

--- readme_generator/tests/test_make_readme.py
+++ readme_generator/tests/test_make_readme.py
@@ -29,8 +29,6 @@ def test_running_make_readme():
         subprocess.check_call(
             [
                 TEST_DIRECTORY.parent / "make_readme.py",
-                "-l",
-                TEST_DIRECTORY.parent.parent.parent,
                 temporary_tested_app_directory,
             ]
         )
